77fe18906c74b02d4f982b78a50f4d2e1f4d1949
Doug <doug@sfdesignerdw.com>
2017-04-11 17:59:46 -0700

    Revert "Backup Commit"
    This reverts commit 2f57c0a34b7b88c04a8602e3e8d37588b0bb5fc2.

--- Ch04/04_10/start/DiceOut/app/src/main/java/com/example/diceout/MainActivity.java
+++ Ch04/04_10/start/DiceOut/app/src/main/java/com/example/diceout/MainActivity.java
@@ -13,8 +13,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -98,17 +96,6 @@ public class MainActivity extends AppCompatActivity {
         dice.add(die1);
         dice.add(die2);
         dice.add(die3);
-
-        for (int dieOfSet = 0; dieOfSet < 3; dieOfSet++) {
-            String imageName = "die_" + dice.get(dieOfSet) + ".png";
-
-            try {
-                InputStream stream = getAssets().open(imageName);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
         // Build message with the result
         String msg = "You rolled a " + die1 + ", a " + die2 + ", and a " + die3;
